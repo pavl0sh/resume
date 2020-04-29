f8d7a871fca3913b245ea8cfc51d577ea39cae15
pavl0sh <duckdogers07@gmail.com>
2020-04-29 20:30:40 +0300

    second section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,12 @@
 import "nes.css/css/nes.min.css"
 import "../styles/fullpage.css"
-import "../styles/styles.css"
+import "../styles/styles.scss"
 import Typed from "typed.js"
 import fullpage from "fullpage.js";
 
 const fullPageInstance = () => {
     new fullpage('#fullpage', {
-        sectionsColor: ['#1BBC9B', '#C63D0F', '#7E8F7C'],
+        sectionsColor: ['#1BBC9B', '#C2B28F', '#83B799', '#E4D8B4'],
         navigation: true,
         navigationPosition: 'right',
         fixedElements: 'nav'
